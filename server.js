d76f6aaea33c8dd8d54d81c46eba44e64eb056f4
Mefiz <nevoscarlet@gmail.com>
2020-02-25 08:32:58 +0500

    testing sending different videos for different ips -- to be implemented: read ip and path from json and update json when file changes

--- server.js
+++ server.js
@@ -1,46 +1,54 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+//home route to send a webpage
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+//function to establish a video stream
+function send_video_stream(filepath, req,res) {
+  const path = filepath;
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(path, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4"
+    };
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4"
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
+}
+
+//send a video to home route
+app.get("/", function(req, res) {
+  let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log(ip);
+  if(ip === '192.168.120.206'){
+    send_video_stream('assets/video_2.mp4', req,res);
+  } else {
+    send_video_stream('assets/sample.mp4', req,res);
+  }
 });
 
-app.get('/video', function(req, res) {
-    const path = 'assets/sample.mp4'
-    const stat = fs.statSync(path);
-    const fileSize = stat.size;
-    const range = req.headers.range;
-
-    if(range) {
-        const parts = range.replace(/bytes=/,"").split("-");
-        const start = parseInt(parts[0],10);
-        const end = parts[1]
-        ? parseInt(parts[1], 10)
-        : fileSize-1;
-
-        const chunksize = (end - start) + 1;
-        const file = fs.createReadStream(path, {start,end});
-        const head = {
-            'Content-Range':`bytes${start}-${end}/${fileSize}`,
-            'Accept-Ranges':'bytes',
-            'Content-Length':chunksize,
-            'Content-Type':'video/mp4,'
-        }
-
-        res.writeHead(206, head);
-        file.pipe(res);
-    } else {
-        const head = {
-            'Content-Length': fileSize,
-            'Content-Type':'video/mp4',
-        }
-        res.writeHead(200, head);
-        fs.createReadStream(path).pipe(res);
-    }
-});
-
-app.listen(3000, function(){
-    console.log('App is running on port 3000');
+app.listen(3000, '0.0.0.0', function() {
+  console.log("App is running on port 3000");
 });
